b9c6751765dc0548e1cc8087c89239e2f946504f
Lino <u201819681@upc.edu.pe>
2020-10-31 10:39:05 -0500

    Production

--- FoodYeahSourceFront/src/environments/environment.prod.ts
+++ FoodYeahSourceFront/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  HOST_URL:'http://localhost:7000',
+  HOST_URL:'https://finanzas-api-kind-oribi-eo.mybluemix.net',
   TOKEN_AUTH_USERNAME:'foodyeahapp',
   TOKEN_AUTH_PASSWORD:'fy89codex',
   TOKEN_NAME:'access_token',
