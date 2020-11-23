83ac6f3c45eff3e9c8b8641b2908fe4e3effd665
Lino <u201819681@upc.edu.pe>
2020-11-23 13:54:51 -0500

    env

--- FutureMarketFront/src/environments/environment.ts
+++ FutureMarketFront/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  HOST_URL:'http://finanzas-api-kind-oribi-eo.mybluemix.net',
+  HOST_URL:'https://finanzas-api-kind-oribi-eo.mybluemix.net',
   TOKEN_AUTH_USERNAME:'foodyeahapp',
   TOKEN_AUTH_PASSWORD:'fy89codex',
   TOKEN_NAME:'access_token',
